79a1e37bc2c8e96905692fcd121792a509e5bd69
Daniel Freudberg <dbfreudber@gmail.com>
2018-02-14 09:26:56 -0500

    finally started on the javascript

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 // create 5 divs (1 for question, 1 for each answer... and i'll push into those)
 // create the words to put in divs to put in the score tracker and to highlight the background of as you answer the correct questions....
 // more psudocode goes here!
+
+// create start button to actually start the game
+//
+
+var mainQuestion = document.querySelector(".question")
+var answerA = document.querySelector(".a")
+var answerB = document.querySelector(".b")
+var answerC = document.querySelector(".c")
+var answerD = document.querySelector(".d")
+
+
+const questions [
+    {
+        question: "1 2 3 4, which number is third?",
+        choices: ["1", "2", "3", "4"],
+        answer: 2
+    },
+    {
+        question: 
+        choices:
+        answer: 
+    }
+]
