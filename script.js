0564b08dba8d02f8f55ce7902cb0b7096a31f524
Daniel Freudberg <dbfreudber@gmail.com>
2018-02-14 13:37:19 -0500

    ummm THINGS WORK. Now to figure out how to highlight my score boxes each time

--- script.js
+++ script.js
@@ -11,55 +11,51 @@ const answerB = document.querySelector('.b')
 const answerC = document.querySelector('.c')
 const answerD = document.querySelector('.d')
 let i = 0
-let someVar = ''
+// let someVar = ''
 
 var questions = [
   {
     question: '1 2 3 4, which number is third?',
     choices: ['1', '2', '3', '4'],
-    answer: 2
+    answer: '3'
   },
   {
     question: 'which number is second, 1234 ',
-    choices: ['1 ', '2', '3', '4'],
-    answer: 1
+    choices: ['4 ', '2', '3', '1'],
+    answer: '2'
   }
 ]
 
-document.addEventListener('keypress', () => {
+document.addEventListener('keypress', quizStart)
+
+function quizStart () {
   mainQuestion.innerHTML = questions[i].question
   answerA.innerHTML = questions[i].choices[0]
   answerB.innerHTML = questions[i].choices[1]
   answerC.innerHTML = questions[i].choices[2]
   answerD.innerHTML = questions[i].choices[3]
-}, {once: true})
-
-function quizStart () {
-  answerA.addEventListener('click', 
-
-//   var finalAnswer = confirm('Final Answer!')
-//   if (finalAnswer == true) {
-//       //check the answer and go to the next question...
-//         } else {
-//           //go back
-//         }
-    })
-
-  answerB.addEventListener('click', () => {
-
-    })
-
-  answerC.addEventListener('click', () => {
-
-    })
-
-  answerD.addEventListener('click', () => {
-
-    })
 }
 
-function clicklyClicky
-// create event listener for each answer
+  // create event listener for each answer
+answerA.addEventListener('click', clickyClicky)
+answerB.addEventListener('click', clickyClicky)
+answerC.addEventListener('click', clickyClicky)
+answerD.addEventListener('click', clickyClicky)
+
+function clickyClicky () {
+  if (this.innerHTML === questions[i].answer) { 
+alert('correct')
+      i++
+      quizStart()
+    }
+  else {
+      alert('WRONG, RESET PAGE')
+    }
+}
+
+// quizStart()
+// clickyClicky () =>
+
 // create event listener
 // create if statement for true or false
 //
